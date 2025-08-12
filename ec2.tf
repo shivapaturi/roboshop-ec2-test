b748ee42e255f85228de4e8a427c5f117ae13ebb
shiva paturi <shivapaturi01@gmail.com>
2025-08-12 23:00:21 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,8 @@
 module "ec2" {
     source = "../terraform-aws-instance"
-    sg_ids = "sg-07079be90cc08029e"
+    sg_ids = ["sg-07079be90cc08029e"]
+
     tags = {
-        Name = "cart"
+        Name = "robosshop-cart"
     }
 }
