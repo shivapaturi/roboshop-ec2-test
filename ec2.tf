2fead153eb991b6171bff88dd9e2a3b2d43ef14a
shiva paturi <shivapaturi01@gmail.com>
2025-08-12 23:14:49 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 module "ec2" {
     source = "../terraform-aws-instance"
     sg_ids = var.security_group_ids
-    instance_type = "t3.medium"
+    instance_type = var.instance_type
     tags = var.tags
 }    
